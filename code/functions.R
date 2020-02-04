6341e33dc90c6a7bd1b9598086b6ceb2d7513f45
tomkoset <tomkoset@umich.edu>
2020-02-04 16:49:54 -0500

    Update since I've corrected duplicates in make_file_fix_ids.batch and fix_ids.R instead

--- code/functions.R
+++ code/functions.R
@@ -14,50 +14,30 @@ metadata <- read_csv("data/process/vendor_metadata.v2.csv") %>%
 shared_sample_names <- read.table('data/process/vendors.subsample.shared', 
                                   sep = '\t', header = T, stringsAsFactors = F) %>% 
   select(Group) # select column with sample names of shared file which we will need to match our metadata to
-#429 samples were sequenced. 
+#404 samples with sequence data. 
 
 #Figure out which samples match/don't match between the shared and metadata files
 matching <- inner_join(metadata, shared_sample_names, by = c('id' = 'Group'))
-#418 samples match between metadata & shared file.
+#404 samples match between metadata & shared file.
 
-# Y22E6E2 should be Y22D6E2. Correct this name on metadata in R so that it will match
-# all mothur files that will be joined to metadata
-metadata <- metadata %>% 
-  mutate(id = replace(id, id == "Y22D6E2", "Y22E6E2"))
-
-#What's missing from metadata compared to shared_sample_names
-missing <- anti_join(shared_sample_names, metadata, by = c('Group' = "id"))
-#11 samples on shared_sample_names that don't correspond to ids listed in the metadata.
-#Previously 14 samples were missing, but since 2 samples were mislabeled in metadata.csv. I Updated the metadata file to fix mislabeled samples in id column.
-#Checked again and number of missing samples dropped to 12 as expected. 
-# Sample ids that were corrected in metadata.csv:
-# 2nd istance of C11D0E2 to C11D1E2
+# 3 mislabeled sample ids that were corrected in metadata.v2.csv:
+# 2nd instance of C11D0E2 to C11D1E2
 # Update S22Dn2E2 to S22Dn1E2 (none of the samples were sequenced at Dn2, likely a typo)
-
-#Some samples were sequenced twice. Checked with Lucas and the sample should be the same, just seqeunced across 2 wells.
-# C21D0E2no2 (duplicate of C21D0E2?, which is present in shared_sample_names (both part of run_1, plate_3) or C21D0E1 which isn't present)
-# E212Dn1E2 (actually E12Dn1E2, which is absent in shared_sample_names. Josh checked his notes from loading the plates and confirmed. 
-# E21D1E2No1 and E21D1E2No2. Sample sequenced twice? Both were part of the same plate on run_1, plate_2.
-# E21Dn1E1no2 (duplicate of E21Dn1E1?, which is present in shared_sample_names). Both a part of run_1, plate_3 and E21Dn1E2 is also present on run_1, plate_3
-# S22D1E2No1 and S22D1E2No2. Sample sequenced twice? Which one to pick...Both a part of run_2, plate_1 and S22D1E2 was also sequenced on run_2, plate_3
-# T12D8E1No1 and T12D8E1No2. Sample sequenced twice? Which one to pick...Both  a part of run_2, plate_3, T12D8E2 is in shared_sample_names but not run_plate_info? 
-
-#Okay to lose T12D13E1. The 1st experiment, we collected 1 last stool sample at D13 from this mouse since it was the only mouse still colonized.
-# Y13D8E2. Okay to lose this sample? There was only a Y13 mouse in the 1st experiment according to the metadata and Y13D8E1 is already listed in shared_sample_names. 
-#Looking back at the plate layout maps Y13D9E1 (Y13_D9_E1) was listed twice, so maybe this sample was really a duplicate of that one.
-#Y13D9E1 shows up in run_plate_info duplicates, while Y13D8E2 is not present in run_plate_info or metadata
+# 2nd instance of Y21D0E2 should actually be  Y21D1E2. 
 
 #Check for any duplicate values in final metadata data frame:
 duplicated <- metadata %>% 
   filter(duplicated(id))
-# One sample was listed twice: Y21D0E2. 2nd instance should actually be  Y21D1E2. 
-# Corrected 2nd instance of Y21D0E2 to Y21D1E2 in the metadata.v2.csv
+#0 duplicated samples in metadata
+
+duplicated <- duplicated(shared_sample_names)
+#0 duplicated samples in shared_sample_names
 
 #Load in ids, MiSeq run info, plate #, and plate locations for the sequenced samples----
 run_plate_info <- read_csv("data/process/vendor_16S_run_plate_info.csv") %>% 
   mutate(id = gsub("_", "", original_id)) %>%  # remove underscores from plate_map id labels to match sample ids used for mothur pipeline
   filter(!str_detect(id, "Mock|Water|NoSample")) # remove rows with Mock, Water, or NoSample ids
-#455 samples based on the plate map files for the 2 MiSeq runs that sequenced vendor project samples
+#453 samples based on the plate map files for the 2 MiSeq runs that sequenced vendor project samples
 
 #Check for duplicated ids based on labels from the plate map
 duplicated <- run_plate_info %>% 
@@ -77,37 +57,46 @@ run1_duplicates <- run_plate_info_duplicates %>% filter(run == "run_1")  #23 sam
 run2_duplicates <- run_plate_info_duplicates %>% filter(run == "run_2")  #23 samples
 inner_join(run1_duplicates, run2_duplicates, by = "id") #23 samples
 #So all 23 of these samples were sequenced twice, once on the 1st MiSeq_run, and a 2nd time on the 2nd MiSeq_Run
+#Discussed these with Josh, he thought these were D8E2 samples instead but since the mouse ids aren't a perfect match
+#between experiements, we can't be confident of the label. Josh said there was not a way to differentiate exp. 1 versus exp. 2
+#He also said after samples were aliquoted into plates the tubes were thrown out.
+#Since we can't be confident of the identies of these sequences, I excluded them from the analysis by removing them from the data/raw/ directory
 
 #Pull out samples with no in name (also represent duplicates)
 run_plate_info_no1or2 <- run_plate_info %>% 
   filter(str_detect(id, "No|no"))
 #Returns 8 samples
 # C21D0E2no2 & E21D1E2No2 are the only pair listed without a corresponding no1
+#All of these were corrected so that both sequences would be attributed to the same sample
+#This was done with code/make_file_fix_ids.batch and fix_ids.R. vendors.files was updated as directed before mothur analysis was continued with code/get_good_seqs.batch
 
 #Check what's shared or different between mothur outputted sample ids in the .shared files and ids from the plate map----
 matching_platemap <- inner_join(run_plate_info, shared_sample_names, by = c('id' = 'Group'))
-#433 samples match between plate_map & shared file.
+#422 samples match between plate_map & shared file.
 matching_platemap_duplicates <- matching_platemap %>%  filter(duplicated(id)) 
 #23 of these matches are duplicates
 
 #What's missing from run_plate_info compared to shared_sample_names----
 missing_platemap <- anti_join(shared_sample_names, run_plate_info, by = c('Group' = "id")) %>% pull(Group)
-#19 samples: "C11D8E2" "C21D8E2" "C22D8E2" "E12D8E2" "E21D8E2" "E22D8E2" "J11D8E2" "J21D8E2" "J22D8E2" "S12D8E2" "S21D8E2" "S22D8E2" "T11D8E2" "T12D8E2" "T21D8E2" "Y12D8E2" "Y13D8E2" "Y21D8E2" "Y22D8E2" 
-#All have corresponding .fastq files in the raw data folder but not a corresponding pair of .fastq files in the BaseCalls folder of either sequencing run
+#5 samples: "E12Dn1E2" "E21D1E2" "S22D1E2" "T12D8E1" "Y22D6E2" 
+#All of these were duplicates/typos that were corrected so that both sequences would be attributed to the same sample in the case of duplicates, or for typos, the sample would match up with the corresponding metadata entry
+#This was done with code/make_file_fix_ids.batch and fix_ids.R. vendors.files was updated as directed before mothur analysis was continued with code/get_good_seqs.batch
 
 #What's missing from shared_sample_names compared to run_plate_info----
 missing_shared_samples <- anti_join(run_plate_info, shared_sample_names, by = c('id' = 'Group')) %>% pull(id)
-#20 samples: "C21Dn1E1" "S21Dn1E2" "C11D0E2" "E21D0E1" "Y11D5E2" "C11D2E2" "C12D2E2" "T11D7E2" "Y21D6E2" "T21D4E2" "T22D7E2" "C22D7E2" "C22D2E2" "Y21D1E2" "Y22D4E2" "Y22D3E2" "S22D3E2ANDT22D3E2" "S21D7E1" "T21D7E1" "S22D3E1"
-#All but "S22D3E2ANDT22D3E2" are found as .fastq files in raw data folder. Suspect these samples were dropped during subsampling?
+#31 samples: "C21Dn1E1" "E212Dn1E2" "S21Dn1E2""E21Dn1E1no2""C21D0E2no2""C11D0E2""E21D0E1""Y11D5E2""S22D1E2No1""C11D2E2""C12D2E2""T11D7E2""Y21D6E2""T21D4E2""T22D7E2""C22D7E2""C22D2E2""S22D1E2No2""Y22E6E2""E21D1E2No2""Y21D1E2""E21D1E2No1""Y22D4E2""Y22D3E2" "S22D3E2ANDT22D3E2" "T12D13E1""S21D7E1""T21D7E1""S22D3E1""T12D8E1No2""T12D8E1No1" 
+# "S22D3E2ANDT22D3E2" sequences were removed from data/raw since it represents sequences from 2 different samples.
+# "T12D13E1"sequences were removed from data/raw since it represents sequences collected 13 days post-infection and was the only mouse with a sample collected at that timepoint.
+# 10 Samples represent corrections made with code/make_file_fix_ids.batch and fix_ids.R. vendors.files was updated as directed before mothur analysis was continued with code/get_good_seqs.batch: "E212Dn1E2" "E21Dn1E1no2" "C21D0E2no2" "S22D1E2No1" "S22D1E2No2" "Y22E6E2" "E21D1E2No2" "E21D1E2No1" "T12D8E1No2""T12D8E1No1"
 # 19 Samples dropped after rarefying to 5437 sequences: "C21Dn1E1" "S21Dn1E2" "C11D0E2" "E21D0E1" "Y11D5E2" "C11D2E2" "C12D2E2" "T11D7E2" "Y21D6E2" "T21D4E2" "T22D7E2" "C22D7E2" "C22D2E2" "Y21D1E2" "Y22D4E2" "Y22D3E2" "S21D7E1" "T21D7E1" "S22D3E1"
 
 #Check if anything is missing from metadata compared to list of samples on run_plate_info
 missing_metadata_v_runplate <- anti_join(run_plate_info, metadata, by = "id") %>% pull(id)
-#11 samples that were previous identified above:  "E212Dn1E2" "E21Dn1E1no2" "C21D0E2no2" "S22D1E2No1" "S22D1E2No2" "E21D1E2No2"       
+#12 samples that were previous identified above:  "E212Dn1E2" "E21Dn1E1no2" "C21D0E2no2" "S22D1E2No1" "S22D1E2No2" "Y22E6E2" "E21D1E2No2"       
 # "E21D1E2No1" "S22D3E2ANDT22D3E2" "T12D13E1" "T12D8E1No2" "T12D8E1No1" 
 #Check for the opposite situation
 missing_runplate_v_metadata <- anti_join(metadata, run_plate_info, by = "id") %>% pull(id)
-#141 samples are in metadata but not in the plate map
+#145 samples are in metadata but not in the plate map. 
 
 #Join run/plate/plate_location info to metadata file----
 run_plate_info <- run_plate_info %>% 
