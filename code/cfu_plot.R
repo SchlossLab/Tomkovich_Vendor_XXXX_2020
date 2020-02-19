897e1a59073a1658459d9aba9355a549d0971eba
tomkoset <tomkoset@umich.edu>
2020-02-19 13:21:50 -0500

    Remove old code that is no longer used

--- code/cfu_plot.R
+++ code/cfu_plot.R
@@ -1,20 +1,5 @@
 source("code/functions.R")
 
-cfu_plot <- metadata %>% select(vendor, day, count1, dilution1, count2, dilution2) %>%
-  mutate(cfu1 = count1 / (20 ^ dilution1), cfu2 = count2 / (20 ^ dilution2)) %>%
-  select(-starts_with("dilution"), -starts_with("count")) %>%
-  gather(key = plate_num, value = cfu, -vendor, -day) %>%
-  group_by(vendor, day) %>% 
-  summarize(mean_cfu = mean(cfu, na.rm = TRUE), sd_cfu = sd(cfu, na.rm = TRUE)) %>%
-  mutate(se_cfu = calc_se(sd_cfu, n()),
-         lower_ci = lower_ci(mean_cfu, se_cfu, n()),
-         upper_ci = upper_ci(mean_cfu, se_cfu, n())) %>%
-  ggplot(aes(x = day, y = mean_cfu, group = vendor, color = vendor)) +
-  geom_line() +
-  geom_point() +
-  #geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci)) +
-  scale_y_log10()
-
 #CFU quantification formula based on protocol used by the Young lab----
 #Noticed 0 is counted as CFU even for dilutions where the -1 dilution was not checked. 
 # If -1 dilution was not plated, we can not say for sure whether C. diff CFU for a mouse is really zero. Thus, zeros for any dilutions greater than the -1 dilution were converted to NAs
