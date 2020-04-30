a6ebee58b2bab4b5aed4ea58da246c0273e76746
tomkoset <tomkoset@umich.edu>
2020-04-30 15:39:17 -0400

    Examine initial communities individually by source and combine adonis results for different days and sources into single tables

--- code/pcoa_stats.R
+++ code/pcoa_stats.R
@@ -57,20 +57,20 @@ variables <- all_variables %>%
   
 #Statistical Analysis----
 
-run_adonis <- adonis(all_dist~ run, strata = variables$mouse_id, data = variables, method = "bray", permutations = 9999)
+run_adonis <- adonis(all_dist~ run, strata = variables$mouse_id, data = variables, permutations = 9999)
 # P = 3e-04
 # R2 = .00638
-exp_adonis <- adonis(all_dist~ experiment, strata = variables$mouse_id, data = variables, method = "bray", permutations = 9999)
+exp_adonis <- adonis(all_dist~ experiment, strata = variables$mouse_id, data = variables, permutations = 9999)
 # P = 1 NS
 # R2 = .01712
-source_adonis <- adonis(all_dist~ source, strata = variables$mouse_id, data = variables, method = "bray", permutations = 9999)
+source_adonis <- adonis(all_dist~ source, strata = variables$mouse_id, data = variables, permutations = 9999)
 # P = 1 NS
 # R2 = .35
-day_adonis <- adonis(all_dist~ day, strata = variables$mouse_id, data = variables, method = "bray", permutations = 9999)
+day_adonis <- adonis(all_dist~ day, strata = variables$mouse_id, data = variables, permutations = 9999)
 # P = 1e-04
 # R2 = .11025
 
-all_adonis <- adonis(all_dist~(source/(unique_cage*experiment*run))*day, strata = variables$mouse_id, data = variables, method = "bray", permutations = 9999)
+all_adonis <- adonis(all_dist~(source/(unique_cage*experiment*run))*day, strata = variables$mouse_id, data = variables, permutations = 9999)
 
 tibble(effects = c("source", "day", "source:unique_cage", "source:run", "source:day", "source:unique_cage:run", "source:unique_cage:day"),
         r_sq = all_adonis$aov.tab$R2[1:7],
@@ -103,12 +103,12 @@ dn1_variables <- tibble(id = attr(dn1_dist, "Labels")) %>%
 
 #Unique cage & source are not completely independent so these variables should be nested along with experiment, which cage is nested within
 #All of these samples were on the same MiSeq run so that variable does not need to be examined
-dn1_adonis <- adonis(dn1_dist~source/(unique_cage*experiment), data = dn1_variables, method = "bray", permutations = 9999)
+dn1_adonis <- adonis(dn1_dist~source/(unique_cage*experiment), data = dn1_variables, permutations = 9999)
 
-tibble(effects = c("source", "source:unique_cage"),
+dn1_results <- tibble(effects = c("source", "source:unique_cage"),
        r_sq = dn1_adonis$aov.tab$R2[1:2],
        p = dn1_adonis$aov.tab$Pr[1:2]) %>% 
-  write_tsv("data/process/adonis_dn1.tsv")
+  mutate(day = -1)
 
 dn1_pcoa <- read_tsv("data/mothur/d-1/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.pcoa.axes") %>%
   select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
@@ -127,12 +127,12 @@ d0_variables <- tibble(id = attr(d0_dist, "Labels")) %>%
 
 #Unique cage & source are not completely independent so these variables should be nested along with experiment, which cage is nested within
 #All of these samples were on the same MiSeq run so that variable does not need to be examined
-d0_adonis <- adonis(d0_dist~source/(unique_cage*experiment), data = d0_variables, method = "bray", permutations = 9999)
+d0_adonis <- adonis(d0_dist~source/(unique_cage*experiment), data = d0_variables, permutations = 9999)
 
-tibble(effects = c("source", "source:unique_cage"),
+d0_results <- tibble(effects = c("source", "source:unique_cage"),
        r_sq = d0_adonis$aov.tab$R2[1:2],
        p = d0_adonis$aov.tab$Pr[1:2]) %>% 
-  write_tsv("data/process/adonis_d0.tsv")
+  mutate(day = 0)
 
 d0_pcoa <- read_tsv("data/mothur/d0/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.pcoa.axes") %>%
   select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
@@ -151,12 +151,12 @@ d1_variables <- tibble(id = attr(d1_dist, "Labels")) %>%
 
 #Unique cage & source are not completely independent so these variables should be nested along with experiment, which cage is nested within
 #All of these samples were on the same MiSeq run so that variable does not need to be examined
-d1_adonis <- adonis(d1_dist~source/(unique_cage*experiment), data = d1_variables, method = "bray", permutations = 9999)
+d1_adonis <- adonis(d1_dist~source/(unique_cage*experiment), data = d1_variables, permutations = 9999)
 
-tibble(effects = c("source", "source:unique_cage"),
+d1_results <- tibble(effects = c("source", "source:unique_cage"),
        r_sq = d1_adonis$aov.tab$R2[1:2],
        p = d1_adonis$aov.tab$Pr[1:2]) %>% 
-  write_tsv("data/process/adonis_d1.tsv")
+  mutate(day = 1)
 
 d1_pcoa <- read_tsv("data/mothur/d1/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.pcoa.axes") %>%
   select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
@@ -165,3 +165,171 @@ d1_pcoa <- read_tsv("data/mothur/d1/vendors.trim.contigs.good.unique.good.filter
   filter(!is.na(axis1)) #Remove all samples that weren't sequenced or were sequenced and didn't make the subsampling cutoff
 
 d1 <- plot_pcoa(d1_pcoa, 1)
+
+#Make combined table of adonis results for D-1, 0, and 1
+rbind(dn1_results, d0_results, d1_results) %>% 
+write_tsv("data/process/adonis_dn1-1.tsv")
+
+#Examine initial day -1 vendor communities separately----
+
+#Function to plot pcoa data for each source of mice at day-1, colored according to experiment with shapes to differentiate cage----
+pcoa_vendor <- function(df, source){
+  plot <- ggplot(df, aes(x=axis1, y=axis2, color = experiment, shape = cage)) +
+    geom_point(size=2, alpha = 0.4) +
+    coord_fixed() + 
+    labs(x="PCoA 1",
+         y="PCoA 2",
+         color= "Experiment",
+         shape = "Cage") +
+    theme_classic()
+  save_plot(filename = paste0("results/figures/pcoa_dn1_", source,".png"), plot)
+}
+
+# Read in thetayc distance matrix that represents Day -1 Schloss samples----
+s_dn1_dist <- read_dist("data/mothur/d-1/schloss/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.dist")
+
+#Extract sample ids from distance matrix and join to metadata in order to test relevant variables
+s_dn1_variables <- tibble(id = attr(s_dn1_dist, "Labels")) %>% 
+  left_join(variables, by = "id") 
+
+#Unique cage & source are not completely independent so these variables should be nested along with experiment, which cage is nested within
+#All of these samples were on the same MiSeq run so that variable does not need to be examined. Generates set of all permutations, which is less than minperm.
+s_dn1_adonis <- adonis(s_dn1_dist~experiment*unique_cage, data = s_dn1_variables, complete=TRUE)
+
+s_results <- tibble(effects = c("experiment", "unique_cage"),
+       r_sq = s_dn1_adonis$aov.tab$R2[1:2],
+       p = s_dn1_adonis$aov.tab$Pr[1:2]) %>% 
+  mutate(source= "Schloss")
+    
+s_dn1_pcoa <- read_tsv("data/mothur/d-1/schloss/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.pcoa.axes") %>%
+  select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
+  rename(id = group) %>% #group is the same as id in the metadata data frame
+  right_join(metadata, by= "id") %>% #merge metadata and PCoA data frames
+  filter(!is.na(axis1)) #Remove all samples that weren't sequenced or were sequenced and didn't make the subsampling cutoff
+
+s_dn1 <- pcoa_vendor(s_dn1_pcoa, "schloss")
+
+# Read in thetayc distance matrix that represents Day -1 Young samples----
+y_dn1_dist <- read_dist("data/mothur/d-1/young/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.dist")
+
+#Extract sample ids from distance matrix and join to metadata in order to test relevant variables
+y_dn1_variables <- tibble(id = attr(y_dn1_dist, "Labels")) %>% 
+  left_join(variables, by = "id") 
+
+#Unique cage & source are not completely independent so these variables should be nested along with experiment, which cage is nested within
+#All of these samples were on the same MiSeq run so that variable does not need to be examined. Generates set of all permutations, which is less than minperm.
+y_dn1_adonis <- adonis(y_dn1_dist~experiment*unique_cage, data = y_dn1_variables, complete=TRUE)
+
+y_results <- tibble(effects = c("experiment", "unique_cage"),
+       r_sq = y_dn1_adonis$aov.tab$R2[1:2],
+       p = y_dn1_adonis$aov.tab$Pr[1:2]) %>% 
+  mutate(source= "Young")
+
+y_dn1_pcoa <- read_tsv("data/mothur/d-1/young/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.pcoa.axes") %>%
+  select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
+  rename(id = group) %>% #group is the same as id in the metadata data frame
+  right_join(metadata, by= "id") %>% #merge metadata and PCoA data frames
+  filter(!is.na(axis1)) #Remove all samples that weren't sequenced or were sequenced and didn't make the subsampling cutoff
+
+y_dn1 <- pcoa_vendor(y_dn1_pcoa, "young")
+
+# Read in thetayc distance matrix that represents Day -1 Jackson samples----
+j_dn1_dist <- read_dist("data/mothur/d-1/jackson/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.dist")
+
+#Extract sample ids from distance matrix and join to metadata in order to test relevant variables
+j_dn1_variables <- tibble(id = attr(j_dn1_dist, "Labels")) %>% 
+  left_join(variables, by = "id") 
+
+#Unique cage & source are not completely independent so these variables should be nested along with experiment, which cage is nested within
+#All of these samples were on the same MiSeq run so that variable does not need to be examined. Generates set of all permutations, which is less than minperm.
+j_dn1_adonis <- adonis(j_dn1_dist~experiment*unique_cage, data = j_dn1_variables, complete=TRUE)
+
+j_results <- tibble(effects = c("experiment", "unique_cage"),
+       r_sq = j_dn1_adonis$aov.tab$R2[1:2],
+       p = j_dn1_adonis$aov.tab$Pr[1:2]) %>% 
+  mutate(source= "Jackson")
+
+j_dn1_pcoa <- read_tsv("data/mothur/d-1/jackson/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.pcoa.axes") %>%
+  select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
+  rename(id = group) %>% #group is the same as id in the metadata data frame
+  right_join(metadata, by= "id") %>% #merge metadata and PCoA data frames
+  filter(!is.na(axis1)) #Remove all samples that weren't sequenced or were sequenced and didn't make the subsampling cutoff
+
+j_dn1 <- pcoa_vendor(j_dn1_pcoa, "jackson")
+
+# Read in thetayc distance matrix that represents Day -1 Charles River samples----
+c_dn1_dist <- read_dist("data/mothur/d-1/charles/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.dist")
+
+#Extract sample ids from distance matrix and join to metadata in order to test relevant variables
+c_dn1_variables <- tibble(id = attr(c_dn1_dist, "Labels")) %>% 
+  left_join(variables, by = "id") 
+
+#Unique cage & source are not completely independent so these variables should be nested along with experiment, which cage is nested within
+#All of these samples were on the same MiSeq run so that variable does not need to be examined. Generates set of all permutations, which is less than minperm.
+c_dn1_adonis <- adonis(c_dn1_dist~experiment*unique_cage, data = c_dn1_variables, complete=TRUE)
+
+c_results <- tibble(effects = c("experiment", "unique_cage"),
+       r_sq = c_dn1_adonis$aov.tab$R2[1:2],
+       p = c_dn1_adonis$aov.tab$Pr[1:2]) %>% 
+  mutate(source= "Charles River")
+
+c_dn1_pcoa <- read_tsv("data/mothur/d-1/charles/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.pcoa.axes") %>%
+  select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
+  rename(id = group) %>% #group is the same as id in the metadata data frame
+  right_join(metadata, by= "id") %>% #merge metadata and PCoA data frames
+  filter(!is.na(axis1)) #Remove all samples that weren't sequenced or were sequenced and didn't make the subsampling cutoff
+
+c_dn1 <- pcoa_vendor(c_dn1_pcoa, "charles_river")
+
+# Read in thetayc distance matrix that represents Day -1 Taconic samples----
+t_dn1_dist <- read_dist("data/mothur/d-1/taconic/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.dist")
+
+#Extract sample ids from distance matrix and join to metadata in order to test relevant variables
+t_dn1_variables <- tibble(id = attr(t_dn1_dist, "Labels")) %>% 
+  left_join(variables, by = "id") 
+
+#Unique cage & source are not completely independent so these variables should be nested along with experiment, which cage is nested within
+#All of these samples were on the same MiSeq run so that variable does not need to be examined. Generates set of all permutations, which is less than minperm.
+t_dn1_adonis <- adonis(t_dn1_dist~experiment*unique_cage, data = t_dn1_variables, complete=TRUE)
+
+t_results <- tibble(effects = c("experiment", "unique_cage"),
+       r_sq = t_dn1_adonis$aov.tab$R2[1:2],
+       p = t_dn1_adonis$aov.tab$Pr[1:2]) %>% 
+  mutate(source= "Taconic")
+
+t_dn1_pcoa <- read_tsv("data/mothur/d-1/taconic/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.pcoa.axes") %>%
+  select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
+  rename(id = group) %>% #group is the same as id in the metadata data frame
+  right_join(metadata, by= "id") %>% #merge metadata and PCoA data frames
+  filter(!is.na(axis1)) #Remove all samples that weren't sequenced or were sequenced and didn't make the subsampling cutoff
+
+t_dn1 <- pcoa_vendor(t_dn1_pcoa, "taconic")
+
+# Read in thetayc distance matrix that represents Day -1 Envigo samples----
+e_dn1_dist <- read_dist("data/mothur/d-1/envigo/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.dist")
+
+#Extract sample ids from distance matrix and join to metadata in order to test relevant variables
+e_dn1_variables <- tibble(id = attr(e_dn1_dist, "Labels")) %>% 
+  left_join(variables, by = "id") 
+
+#Unique cage & source are not completely independent so these variables should be nested along with experiment, which cage is nested within
+#All of these samples were on the same MiSeq run so that variable does not need to be examined. Generates set of all permutations, which is less than minperm.
+e_dn1_adonis <- adonis(e_dn1_dist~experiment*unique_cage, data = e_dn1_variables, complete=TRUE)
+
+e_results <- tibble(effects = c("experiment", "unique_cage"),
+       r_sq = e_dn1_adonis$aov.tab$R2[1:2],
+       p = e_dn1_adonis$aov.tab$Pr[1:2]) %>% 
+  mutate(source= "Envigo")
+
+e_dn1_pcoa <- read_tsv("data/mothur/d-1/envigo/vendors.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.pcoa.axes") %>%
+  select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
+  rename(id = group) %>% #group is the same as id in the metadata data frame
+  right_join(metadata, by= "id") %>% #merge metadata and PCoA data frames
+  filter(!is.na(axis1)) #Remove all samples that weren't sequenced or were sequenced and didn't make the subsampling cutoff
+
+e_dn1 <- pcoa_vendor(e_dn1_pcoa, "envigo")
+
+#Merge adonis results for each source of mice together to create one final results table
+rbind(s_results, y_results, j_results, c_results, t_results, e_results) %>% 
+write_tsv("data/process/adonis_dn1_source.tsv")
+
