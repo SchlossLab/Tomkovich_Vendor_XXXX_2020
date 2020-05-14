b1094e3fbe9e8878a506c91c5bff7aeb6874a8bd
tomkoset <tomkoset@umich.edu>
2020-05-14 11:37:18 -0400

    Reorder figures and figure labels

--- code/figure_3.R
+++ code/figure_3.R
@@ -1,14 +1,11 @@
 source("code/functions.R")
 
-a <- ggdraw() + draw_image("results/figures/richness_dn1.png")
-b <- ggdraw() + draw_image("results/figures/richness_d0.png")
-c <- ggdraw() + draw_image("results/figures/richness_d1.png")
-d <- ggdraw() + draw_image("results/figures/shannon_dn1.png")
-e <- ggdraw() + draw_image("results/figures/shannon_d0.png")
-f <- ggdraw() + draw_image("results/figures/shannon_d1.png")
+a <- ggdraw() + draw_image("results/figures/pcoa_day-1.png")
+b <- ggdraw() + draw_image("results/figures/pcoa_day0.png")
+c <- ggdraw() + draw_image("results/figures/pcoa_day1.png")
 
+plot_grid(a, b, c, labels = "AUTO", label_size = 12, ncol=1)+
+  ggsave("results/figures/figure_3.pdf", width=3.4, height=8.5)+
+  ggsave("submission/figure_3.pdf", width=3.4, height=8.5)
 
-plot_grid(a, b, c, d, e, f, labels = "AUTO", label_size = 12, ncol=3)+
-  ggsave("results/figures/figure_3.pdf", width=6, height=4)+
-  ggsave("submission/figure_3.pdf", width=6, height=4)
 
