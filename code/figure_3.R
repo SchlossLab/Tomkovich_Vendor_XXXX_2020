af152711942b47c2f4ff76137ff0be2ff557a6b9
tomkoset <tomkoset@umich.edu>
2020-08-01 20:10:07 -0400

    Reorder D-E

--- code/figure_3.R
+++ code/figure_3.R
@@ -4,8 +4,8 @@ a <- ggdraw() + draw_image("results/figures/richness_d0.png")
 b <- ggdraw() + draw_image("results/figures/shannon_d0.png")
 c <- ggdraw() + draw_image("results/figures/pcoa_day0.png")
 top_row <- plot_grid(a, b, c, labels = "AUTO", label_size = 12, nrow=1)
-d <- ggdraw() + draw_image("results/figures/clind_impacted_otus_plot.png")
-e <- ggdraw() + draw_image("results/figures/D0top18_otus.png")
+d <- ggdraw() + draw_image("results/figures/D0top18_otus.png")
+e <- ggdraw() + draw_image("results/figures/clind_impacted_otus_plot.png")
 bottom_row <- plot_grid(d, e, labels = c("D", "E"), label_size = 12, nrow=1)
 
 plot_grid(top_row, bottom_row, labels = c("", ""), label_size = 12, ncol=1)+
